235e121f9bbf377b1006b470bcb29aea0bc67869
999010alex <alexandercastillo067@gmail.com>
2016-12-22 14:49:48 -0500

    Create december22cw.py

---
+++ december22cw.py
@@ -0,0 +1,89 @@
+'''
+1
+'''
+balance=100
+intrest=1.1
+print(balance*intrest**7)
+'''
+2
+'''
+
+savings=100
+factor=1.10
+print(savings*factor**7)
+result=194.8717
+'''
+3
+'''
+string="compound interest"
+boolean=True
+'''
+4
+'''
+
+
+
+'''
+5
+'''
+print('I started with $100, and now I have $194.87, great!')
+'''
+1
+'''
+print("Hello","World","!")
+'''
+2
+'''
+#You get a syntax error because "World! is not closed off.
+'''
+3
+'''
+#print("Helu","word","?")
+#print(hello world!)
+#print("HELLO WORLD!"
+'''
+4
+'''
+
+'''
+5
+'''
+print("Guten tag!")
+'''
+6
+'''
+print(1+2+3+4+5+6+7+8+9+10)
+'''
+7
+'''
+print(1*2*3*4*5*6*7*8*9*10)
+'''
+8
+'''
+balance=1000
+intrest=1.05
+print(balance*intrest)
+print(balance*intrest**2)
+print(balance*intrest**3)
+'''
+9
+'''
+print(' ______\n| Alex |\n ------' )
+'''
+10
+'''
+print('   **      *     *****    *       *')
+print('  *  *     *     *          *   *  ')
+print(' ******    *     *****        *    ')
+print('*      *   *     *          *   *  ')
+print('*      *   ****  *****    *       *')
+'''
+11
+'''
+print('  \\\//  ')
+print(' +"""""+ ')
+print('(|o   o|)')
+print(' |  >  | ')
+print(' | (-) | ')
+print(' |     | ')
+print(' +-----+ ')
